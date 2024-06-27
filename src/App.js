961d74d4fe61fed9d72904b1ee2d65be9ef8af02
Phetho <156676555+Phetho16@users.noreply.github.com>
2024-06-27 08:12:06 +0200

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
-import Header from "./components/layout/Header";
-import Home from "./components/layout/Home";
-import Footer from "./components/layout/Footer";
-import SearchPage from "./components/layout/SearchPage";
-import Modal from "./components/layout/Modal"
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './components/layout/Header';
+import Home from './components/layout/Home';
+import Footer from './components/layout/Footer';
+import SearchPage from './components/layout/SearchPage';
+import Modal from './components/layout/Modal';
 
 const App = () => {
   return (
     <div className="app">
       <Router>
         <Header />
-        <Switch>
-          <Route path="/" exact>
-            <Home />
-          </Route>
-          <Route path="/search">
-            <SearchPage />
-          </Route>
-        </Switch>
-        <Route path= "/">
-          <Modal/>
-        </Route>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/" element={<Modal />} />
+        </Routes>
         <Footer />
       </Router>
     </div>
