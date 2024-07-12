e0606c259048c7bee25c3983374d7dcb03e7139b
Phetho <156676555+Phetho16@users.noreply.github.com>
2024-07-12 21:41:58 +0200

    fixed all the bugs

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/layout/Home';
 import Footer from './components/layout/Footer';
 import SearchPage from './components/layout/SearchPage';
 import Modal from './components/layout/Modal';
+import Login from './components/Login';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<SearchPage />} />
           <Route path="/" element={<Modal />} />
+          <Route path='/login' element={<Login/>} />
         </Routes>
         <Footer />
       </Router>
--- src/components/layout/Home.js
+++ src/components/layout/Home.js
@@ -1,68 +1,84 @@
-import React from "react";
-// import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Banner from "./Banner";
 import Card from "./Card";
 import "./Home.css";
-// import {listListing} from "./action/listingAction" 
+import { listListing } from "./action/listingAction";
 
 const Home = () => {
-//   const dispatch = useDispatch();
-//   const listingList = useSelector(state => state.listingList)
-//   const {loading, error, listings} = listingList
+  const dispatch = useDispatch();
+  const listingList = useSelector((state) => state.listingList);
+  const { loading, error, listings } = listingList;
 
-//   useEffect(() =>{
-//     dispatch(listListing())
-//   }, [dispatch])
+  useEffect(() => {
+    dispatch(listListing());
+  }, [dispatch]);
   return (
     <div className="home">
       <Banner />
-     <div className="home_section">
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <div className="home_section">
+          {listings.map((listing) => (
+            <Card
+              src={listing.img}
+              title={listing.title}
+              description={listing.description}
+              price={listing.price}
+            />
+          ))}
+        </div>
+      )}
+      <div className="home_section">
         <Card
-          src="https://a0.muscache.com/im/pictures/miso/Hosting-5264493/original/10d2c21f-84c2-46c5-b20b-b51d1c2c971a.jpeg?im_w=320"
-          title="Moinho das Feteiras | The Mill"
-          description="Windmill in Ponta Delgada, Portugal"
+          src="https://a0.muscache.com/im/pictures/monet/Luxury-684763757047711680/original/48161af7-0e24-4d39-a187-b45ea277d49f?im_w=720"
+          title="Iron Man House"
+          description="Entire villa in Cape Town, South Africa"
         />
 
         <Card
-          src="https://a0.muscache.com/im/pictures/miso/Hosting-676044415326884478/original/e32429f8-56bd-4ba1-a98a-46efc64e3e69.jpeg?im_w=320"
-          title="OMG Winner, Coconest Langkawi"
-          description="Campsite in Langkawi, Malaysia"
+          src="https://a0.muscache.com/im/pictures/a3a11849-ed9c-4674-8546-d210f0245496.jpg?im_w=720"
+          title="Hollywood Mansion"
+          description="Entire villa in Cape Town, South Africa"
         />
         <Card
-          src="https://a0.muscache.com/im/pictures/6dcdc10d-400a-4ae9-b811-d3271655ecf4.jpg?im_w=320"
-          title="Driftwood Treehouse by the Sea"
-          description="Treehouse in East London, South Africa"
+          src="https://a0.muscache.com/im/pictures/miso/Hosting-655743318495785740/original/395456e1-517c-40e5-b42a-7389e8b54ed8.jpeg?im_w=720"
+          title="Luxury, Modern Lifestyle, Amazing Views, Camps Bay"
+          description="Entire rental unit in Cape Town, South Africa"
         />
         <Card
-          src="https://a0.muscache.com/im/pictures/miso/Hosting-917780628259593306/original/f273fcbe-f1de-48b5-b28d-fa247e2d5646.jpeg?im_w=320"
-          title="NOCK | Private Cabin with a Breathtaking Bay View"
-          description="Entire cabin in Ghosta, Lebanon"
+          src="https://a0.muscache.com/im/pictures/a3e07d96-169a-41a4-81e1-e02109a28bfe.jpg?im_w=720"
+          title="Grand Villa"
+          description="Entire villa in Thalang, Thailand"
         />
       </div>
       <div className="home_section">
         <Card
-          src="https://a0.muscache.com/im/pictures/55cde044-a7de-4b33-aff1-3692301e86c9.jpg?im_w=320"
-          title="CocoHut: Recharge in Nature on a Regenerative Farm"
-          description="Entire villa in Predeal, Romania"
+          src="https://a0.muscache.com/im/pictures/monet/Luxury-640045233317855589/original/a48a58a2-d1d3-4d09-8ad5-fdb08507363f?im_w=720"
+          title="Villa Buena"
+          description="Entire villa in Illes Balears, Spain"
           price="R6,119 ZAR/ night"
         />
         <Card
-          src="https://a0.muscache.com/im/pictures/miso/Hosting-53356466/original/f353fb80-0915-40ab-8032-6bc810ac273e.jpeg?im_w=720"
-          title="Vila Sonja - Fairytale Villa - Magic Location"
-          description="Campsite in Langkawi, Malaysia"
-          price="R15,327 ZAR/ night"
+          src="https://a0.muscache.com/im/pictures/monet/Luxury-602569246153730946/original/7c5c7ae3-14c2-4539-ab85-e3ee40867fec?im_w=720"
+          title="Luxury villa Supreme Rogoznica 1 with pool"
+          description="Entire villa in Rogoznica, Croatia"
+          price="R42,146 ZAR/ night"
         />
 
         <Card
-          src="https://a0.muscache.com/im/pictures/c579efd0-3a7a-4132-8bbb-cf7eeadc9806.jpg?im_w=320"
-          title="Aura House 2bds Eco Bamboo House, Pool, River View"
-          description="Entire villa in Abiansemal, Indonesia"
-          price="R9,549/ night ZAR"
+          src="https://a0.muscache.com/im/pictures/miso/Hosting-873069117835483193/original/64e68a05-f5e9-46ca-a3ef-7952bb4fa3de.jpeg?im_w=720"
+          title="Trisara"
+          description="Entire place in Bharje, India"
+          price="R178,715/ night ZAR"
         />
         <Card
-          src="https://a0.muscache.com/im/pictures/a84d991e-8d4a-4ff6-818b-ca07292d3343.jpg?im_w=320"
-          title="Inaugural offer- Cavehouse with private pool"
-          description="Cave in Kalpetta, India"
+          src="https://a0.muscache.com/im/pictures/miso/Hosting-37922090/original/3e98dbd8-1b21-49b2-afc7-f1a227470804.jpeg?im_w=720"
+          title="Luxury Villa Skyfall Phuket"
+          description="Entire villa in Tambon Ratsada, Thailand"
           price="R2,606 ZAR/ night"
         />
       </div>
